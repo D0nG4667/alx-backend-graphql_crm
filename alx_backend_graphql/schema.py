44500e0f5b908bd46b143e74fa6811fc86ca7ddc
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-12-15 01:08:02 +0300

    fix: schema directory and requirement

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,5 +1,9 @@
 import graphene
+from crm.schema import Query as CRMQuery
 
-class Query(graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     # Declare the field
     hello = graphene.String(default_value="Hello, GraphQL!")
+
+# Instantiate the schema
+schema = graphene.Schema(query=Query)
